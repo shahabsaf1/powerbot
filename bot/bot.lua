658bfa960eff2ca0f32eb24765c6894a1406b503
Shahab <safaeishahab@yahoo.com>
2016-01-04 21:21:54 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {125130911,121189712},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
